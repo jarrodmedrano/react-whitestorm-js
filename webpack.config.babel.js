1c86e7847ced1b6e4e2c0d94f176c15b13ddf708
jmedran <jmedran@gmail.com>
2017-07-16 17:32:25 -0700

    locking version numbers

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -34,7 +34,7 @@ export default {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
             presets: ['es2015', 'react', 'stage-1'],
